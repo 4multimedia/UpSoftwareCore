7b6b2f761e719d32d03e170b320bff2a7c889594
upsoftware.tech <info@upsoftware.tech>
2024-11-14 11:34:58 +0100

    1.0.31

--- src/Classes/ModelFilter.php
+++ src/Classes/ModelFilter.php
@@ -2,13 +2,19 @@
 
 namespace Upsoftware\Core\Classes;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
 
 class ModelFilter
 {
-    protected $modelClass;
-    protected $query;
-    private $pagination = true;
+    protected Model $modelClass;
+    protected Builder $query;
+    private bool $pagination = true;
+    private $columns;
+    private $joinColumns = [];
+    private $joinTable;
+    private $joinCondition;
 
     public function __construct($modelClass)
     {
@@ -18,10 +24,21 @@ class ModelFilter
 
         $this->modelClass = $modelClass;
         $this->query = $modelClass::query();
+        $this->columns = Schema::getColumnListing((new $modelClass)->getTable());
     }
 
     public function pagination($pagination) {
         $this->pagination = $pagination;
+        return $this;
+    }
+
+    public function join($table, $foreignKey, $localKey)
+    {
+        $this->joinTable = $table;
+        $this->joinCondition = [$foreignKey, '=', $localKey];
+        $this->query->join($table, ...$this->joinCondition);
+
+        $this->joinColumns = Schema::getColumnListing($table);
         return $this;
     }
 
@@ -37,14 +54,29 @@ class ModelFilter
     }
 
     public function search() {
+
+        $request = request()->all();
+
+        foreach ($request as $key => $value) {
+            if (in_array($key, $this->columns)) {
+                $this->query->where($this->modelClass::getTable() . '.' . $key, 'LIKE', "%$value%");
+            } elseif (in_array($key, $this->joinColumns)) {
+                $this->query->where($this->joinTable . '.' . $key, 'LIKE', "%$value%");
+            }
+        }
+
         $rowsPerPage = request()->input('rows_per_page', 10);
         $orderBy = request()->input('order_by', 'name');
         $direction = request()->descending === 'true' ? 'DESC' : 'ASC';
 
-        $this->query->orderBy($orderBy, $direction);
+        if (in_array($orderBy, $this->columns)) {
+            $this->query->orderBy($this->modelClass::getTable() . '.' . $orderBy, $direction);
+        } elseif (in_array($orderBy, $this->joinColumns)) {
+            $this->query->orderBy($this->joinTable . '.' . $orderBy, $direction);
+        }
 
         if ($this->pagination) {
-            return $this->query->paginate($rowsPerPage);
+            return $this->query->paginate($rowsPerPage, 100);
         } else {
             return $this->query->get();
         }
